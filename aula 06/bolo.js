8c058fedeaa3b696ceafe5391f14a95e8e4ecd6d
Lucas Alencar Fernandes <lucasdealencarfernandes@gmail.com>
2025-09-30 21:37:35 -0300

    Guardando as atividades desenvolvidas desde o inicio do curso

---
+++ aula 06/bolo.js
@@ -0,0 +1,16 @@
+let doce = "bolo"
+let usuario = "Jose Silva"
+let precoDoce = 5.20
+
+console.log("doce");
+console.log("usuario");
+console.log("Compre um bolo ou morra");
+console.log(doce + usuario);
+
+console.log("Boa noite,"+ usuario +"! compre um(a)" + doce + "ou morra!");
+
+//quando quero conectar duas frases/strings, posso usar o sinal de + dentro de un log
+
+console.log("Boa noite,"+usuario +": Quero 3" +doce+"s, por favor!");
+
+console.log(usuario+ "valor total da venda ficou: "+ (precoDoce *3).toFixed(20) );
